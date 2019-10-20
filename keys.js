c2a6623136bfc52ba7a7c52f7ba7370cab0729a4
Sara Gryka <sarakgryka@hotmail.com>
2019-10-20 16:14:50 -0700

    bandsintown functioning

--- keys.js
+++ keys.js
@@ -4,3 +4,10 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+exports.bandsInTown = {
+
+    id: process.env.app_id
+
+};
--- liri.js
+++ liri.js
@@ -1,14 +1,66 @@
 require("dotenv").config();
+var axios = require("axios");
 
 var keys = require("./keys.js");
+var artist;
+
 
 // var spotify = new Spotify(keys.spotify);
 
-let userInput = process.argv[2];
+var bandsInTown = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${keys.bandsInTown.id}`
+
+var userInput = process.argv[2];
+var arguments = process.argv;
+var userSearch = "";
+
+
+for (let i = 3; i < arguments.length; i++) {
+
+    if (i > 3 && i < arguments.length) {
+        userSearch = userSearch + "+" + arguments[i];
+    } 
+    
+    
+    else {
+        userSearch += arguments[i];
+
+    }
+
+}
 
 switch (userInput) {
     case "concert-this":
-        console.log("bandsintown");
+        console.log(userSearch);
+
+        artist = userSearch;
+        console.log(artist);
+
+        var bandsInTown = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${keys.bandsInTown.id}`
+
+
+        axios
+            .get(bandsInTown)
+            .then(function (response) {
+
+                for (let i = 0; i < response.data.length; i++) {
+
+                    console.log(`Venue: ${response.data[i].venue.name}`);
+                    console.log(`Venue Location: ${response.data[i].venue.city}`);
+                    console.log(`Date of Event: ${response.data[i].venue.datetime}`);
+                    console.log("========================================================================")
+
+
+                }
+
+
+            })
+            .catch(function (error) {
+
+                return console.log(error);
+            })
+
+
+
         break;
 
     case "spotify-this-song":
